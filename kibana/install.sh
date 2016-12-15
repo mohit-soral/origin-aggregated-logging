09ece0266ff51ca6721d90360b4b9f07789dfe82
mohit-soral <mohitsoral.87@gmail.com>
2016-12-15 16:31:15 +0530

    Changes for updation of version to 5.x
    Not using the AOP Plugin
    Using the correct paths for the config file and Kibana Home

--- kibana/install.sh
+++ kibana/install.sh
@@ -1,13 +1,13 @@
-mkdir -p -m 755 ${KIBANA_HOME}/installedPlugins/origin-kibana
-cd ${KIBANA_HOME}/installedPlugins/origin-kibana
-wget -q ${AOP_KIBANA_PLUGIN_REPO}/releases/download/${AOP_KIBANA_PLUGIN_VER}/origin-kibana-${AOP_KIBANA_PLUGIN_VER}.tgz
-tar -xvzf origin-kibana-${AOP_KIBANA_PLUGIN_VER}.tgz
-rm origin-kibana-${AOP_KIBANA_PLUGIN_VER}.tgz
+#mkdir -p -m 755 ${KIBANA_HOME}/installedPlugins/origin-kibana
+#cd ${KIBANA_HOME}/installedPlugins/origin-kibana
+#wget -q ${AOP_KIBANA_PLUGIN_REPO}/releases/download/${AOP_KIBANA_PLUGIN_VER}/origin-kibana-${AOP_KIBANA_PLUGIN_VER}.tgz
+#tar -xvzf origin-kibana-${AOP_KIBANA_PLUGIN_VER}.tgz
+#rm origin-kibana-${AOP_KIBANA_PLUGIN_VER}.tgz
 
 # Kibana starts up slowly because it tries to optimize and cache bundles
 # so we start it up as part of install and then stop it
-mv ${KIBANA_HOME}/config/kibana.yml ${KIBANA_HOME}/config/hidden_kibana.yml
-touch ${KIBANA_HOME}/config/kibana.yml
+mv ${KIBANA_CONF}/kibana.yml ${KIBANA_CONF}/hidden_kibana.yml
+touch ${KIBANA_CONF}/kibana.yml
 
 ${KIBANA_HOME}/bin/kibana > ${KIBANA_HOME}/kibana.out &
 pid=$!
@@ -18,7 +18,8 @@ done
 kill $pid
 
 rm ${KIBANA_HOME}/kibana.out
-rm ${KIBANA_HOME}/config/kibana.yml
-mv ${KIBANA_HOME}/config/hidden_kibana.yml ${KIBANA_HOME}/config/kibana.yml
+rm ${KIBANA_CONF}/kibana.yml
+mv ${KIBANA_CONF}/hidden_kibana.yml ${KIBANA_HOME}/config/kibana.yml
 chmod -R og+w ${HOME}
 chmod -R og+rw ${KIBANA_HOME}
+chmod -R og+rw ${KIBANA_CONF}
