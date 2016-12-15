b62a2f402290affe3c18356d9e87666cf3deb9b2
mohit-soral <mohitsoral.87@gmail.com>
2016-12-15 16:33:03 +0530

    Updating version to 5.x
    Path of the kibana.yml file updated according to the new version

--- kibana/run.sh
+++ kibana/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -euo pipefail
 
-sed -i "s/es_host/$ES_HOST/" ${KIBANA_HOME}/config/kibana.yml
-sed -i "s/es_port/$ES_PORT/" ${KIBANA_HOME}/config/kibana.yml
+sed -i "s/es_host/$ES_HOST/" ${KIBANA_CONF}/kibana.yml
+sed -i "s/es_port/$ES_PORT/" ${KIBANA_CONF}/kibana.yml
 
 exec ${KIBANA_HOME}/bin/kibana
